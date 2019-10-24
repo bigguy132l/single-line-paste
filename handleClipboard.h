04ba0269120b24274e3f66560c06ee7a0144b3f0
narukaze <bigguy132l@gmail.com>
2019-10-24 11:58:11 -0400

    Added some non-Unicode support code

--- handleClipboard.h
+++ handleClipboard.h
@@ -2,7 +2,11 @@
 
 #include <string>
 
+#ifdef UNICODE
 using chosen_string = std::wstring;
+#else
+using chosen_string = std::string;
+#endif
 
 chosen_string __stdcall getClipboardText();
 void __stdcall setClipboardText(const chosen_string& data);
